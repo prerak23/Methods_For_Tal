92c524f54be1e3b3d587dafb6401d3303dceb651
Prerak Srivastava <psrivastava@fnancy>
2018-12-27 20:16:19 +0100

    Checking

--- plot.py
+++ plot.py
@@ -7,15 +7,21 @@ string = re.sub("/[\[\]']","",string)
 output=string.split(",")
 counter=0
 output_data=[]
+epoch=[]
+epoch_value=1
 for data in output:
-    if counter > 1 and  "'0101010'" not in data:
-        
-        output_data.append(float(data))
+    if counter > 1 :
+      if  "'0101010'" in data:
+          epoch_value=epoch_value+1
+          counter=1
+      else:
+          output_data.append(float(data))
+          epoch.append(epoch_value)
     counter+=1
 plt.title("loss as a function of epoch")
 plt.xlabel("epoch")
 plt.ylabel("loss")
-plt.plot([x  for x in range(len(output_data))],output_data)
+plt.plot(output_data,epoch)
 plt.savefig("loss_as_whole")
 print(output_data)
     
